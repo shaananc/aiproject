17bd716423d332a939384015fa2085318e3e1c02
Shaanan Cohney <shaananc@gmail.com>
2013-08-26 22:58:13 +1000

    Added to play human

--- src/gameai/NegaScout.java
+++ src/gameai/NegaScout.java
@@ -6,6 +6,7 @@ package gameai;
 
 import java.io.ByteArrayInputStream;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  *
@@ -14,26 +15,30 @@ import java.util.List;
 public class NegaScout {
 
     int maxdepth = 2;
+    List<Move> curbest;
 
     int Evaluate(gameBoard gb, List<Move> m) {
 
-        int[] numbers = gb.getNumbers();
-        int ret;
-        
+        System.out.println("evaluating:");
+        System.out.println(gb);
 
-        return m.get(0).x;
-        
-        //if (gb.turn != gameBoard.WHITE) {
-        //     ret = numbers[0] - numbers[1];
-        //} else {
-        //    ret = numbers[1] - numbers[0];
-        //}
-        
-        //System.out.println(gb);
-        //System.out.println("Ret: " + ret + " " + Integer.toString(numbers[0]) + " " + Integer.toString(numbers[1]) + "\n");
-        
-        
-        //return ret;
+        int[] numbers = gb.getNumbers();
+        int ret = numbers[1] - numbers[0];
+
+
+        //return m.get(0).x;
+
+        if (gb.turn != gameBoard.WHITE) {
+            ret = ret * -1;
+        } else {
+            ret = ret;
+        }
+
+
+        System.out.println("Ret: " + ret + " " + Integer.toString(numbers[0]) + " " + Integer.toString(numbers[1]) + "\n");
+
+
+        return ret;
     }
 
     // TODO - evaluate use of depth.
@@ -45,21 +50,22 @@ public class NegaScout {
             return Evaluate(gb, p);
         }
 
-        List<List <Move>> successors = gb.getMoves();
-        
+        List<List<Move>> successors = gb.getMoves();
 
-        
+
+
         a = alpha;
         b = beta;
 
         i = 1;
-        for (List <Move> moveList : successors) {
-            t = -negascout(gb.executeCompound(moveList), moveList, -b, -a,d+1);
+        for (List<Move> moveList : successors) {
+            t = -negascout(gb.executeCompound(moveList), moveList, -b, -a, d + 1);
             if ((t > a) && (t < beta) && (i > 1) && (d < maxdepth - 1)) {
-                a = -negascout(gb.executeCompound(moveList), moveList, -beta, -t,d+1);
+                a = -negascout(gb.executeCompound(moveList), moveList, -beta, -t, d + 1);
             }
             a = Math.max(a, t);
             if (a >= beta) {
+                curbest = p;
                 return a;
             }
             b = a + 1;
@@ -68,23 +74,44 @@ public class NegaScout {
 
     }
 
+    public List<Move> chooseMove(gameBoard gb) {
+
+        System.out.println(negascout(gb, null, Integer.MIN_VALUE, Integer.MAX_VALUE, 1));
+
+        return curbest;
+
+    }
+
     public static void main(String[] args) {
+//        String boardString = "5\n"
+//                + "- - - - -\n"
+//                + "- W W - -\n"
+//                + "- B - - -\n"
+//                + "- - - - -\n";
+        playHuman();
+    }
+
+    public static void playHuman() {
         NegaScout ns = new NegaScout();
-        
-        String boardString = "5\n"
-                + "- - - - -\n"
-                + "- W W - -\n"
-                + "- B - - -\n"
-                + "- - - - -\n";
-        
-        gameBoard gb = new gameBoard(new ByteArrayInputStream((boardString.toString()).getBytes()));
+        gameBoard gb = new gameBoard(2);
         System.out.println(gb);
-        List<List <Move>> moves = gb.getMoves();
-        for (List<Move> m : moves) {
-            gameBoard c = gb.deepCopy();
-            System.out.println(c.executeCompound(m));
-            System.out.println(ns.negascout(c, m, Integer.MIN_VALUE, Integer.MAX_VALUE,1));
+
+        Scanner s = new Scanner(System.in);
+        while (!s.hasNext("EOF")) {
+            int x = s.nextInt();
+            int y = s.nextInt();
+            Move m = new Move(x, y);
+            gb = gb.executeMove(m);
+            System.out.println(gb);
+            List<Move> e = ns.chooseMove(gb);
+            if (e == null) {
+                continue;
+            }
+            gb = gb.executeCompound(e);
+            System.out.println(gb);
         }
+
+
 
     }
 }

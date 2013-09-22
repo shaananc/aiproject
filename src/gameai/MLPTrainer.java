dd28cb3f8effc84d86a6b6c5c9e013db4920e1db
shaananc <shaananc@SHAANAN-RAZER>
2013-09-22 12:03:53 -0700

    Improved Trainer

--- src/gameai/MLPTrainer.java
+++ src/gameai/MLPTrainer.java
@@ -29,16 +29,22 @@ public class MLPTrainer {
         Player NP = (Player) new NegaPlayer();
         Player MitchP = (Player) new Mbrunton();
 
+
         int i = 1;
-        while (i <= 100) {
-            if (i % 10 == 0) {
+        while (i <= 100000) {
+            if (i % 1000 == 0) {
                 trainer.myMLPPlayer.isLearning = false;
                 trainer.myMLPPlayer2.isLearning = false;
 
-                trainer.playWithOutput(MLP1, NP);
+                
+                System.out.println("Versus NegaPlayer: " + trainer.playWithOutput(MLP1, NP));
+                System.out.println("Versus MitchPlayer: " + trainer.playWithOutput(MLP1, MitchP));
+                trainer.playWithOutput(MLP1, MitchP);
 
                 trainer.myMLPPlayer2.isLearning = true;
                 trainer.myMLPPlayer.isLearning = true;
+
+                System.out.println(i);
 
 
             } else {
@@ -49,9 +55,11 @@ public class MLPTrainer {
                 //trainer.playGame(MitchP, MLP1);
             }
 
-            if (i % 2 == 0) {
+            if (i % 10 == 0) {
                 EncogDirectoryPersistence.saveObject(new File("encognn.eg"), trainer.myMLPPlayer.network);
             }
+
+
             System.out.flush();
             i++;
         }
@@ -109,12 +117,12 @@ public class MLPTrainer {
         }
     }
 
-    public void playWithOutput(Player P1, Player P2) {
+    public String playWithOutput(Player P1, Player P2) {
 
         lastPlayedMove = new Move();
         int NumberofMoves = 0;
         int boardEmptyPieces = n * n;
-        System.out.println("Referee started !");
+        //System.out.println("Referee started !");
 
         P1.init(n, WHITE);
         P2.init(n, BLACK);
@@ -125,13 +133,13 @@ public class MLPTrainer {
             NumberofMoves++;
             lastPlayedMove = P1.makeMove();
             if (lastPlayedMove.IsPlaceMove) {
-                System.out.println("Placing to. " + lastPlayedMove.RowPositions[0] + ":" + lastPlayedMove.ColPositions[0] + " by " + lastPlayedMove.P);
+                //System.out.println("Placing to. " + lastPlayedMove.RowPositions[0] + ":" + lastPlayedMove.ColPositions[0] + " by " + lastPlayedMove.P);
                 boardEmptyPieces--;
             } else {
-                System.out.println("Jumpping from. " + lastPlayedMove.RowPositions[0] + ":" + lastPlayedMove.ColPositions[0] + " by " + lastPlayedMove.P);
+                //System.out.println("Jumpping from. " + lastPlayedMove.RowPositions[0] + ":" + lastPlayedMove.ColPositions[0] + " by " + lastPlayedMove.P);
                 for (int i = 1; i < lastPlayedMove.ColPositions.length; i++) {
                     boardEmptyPieces--;
-                    System.out.println("To. " + lastPlayedMove.RowPositions[i] + ":" + lastPlayedMove.ColPositions[i]);
+                    //System.out.println("To. " + lastPlayedMove.RowPositions[i] + ":" + lastPlayedMove.ColPositions[i]);
                 }
 
             }
@@ -146,8 +154,8 @@ public class MLPTrainer {
                 System.exit(1);
             }
 
-            P1.printBoard(System.out);
-            P2.printBoard(System.out);
+            //P1.printBoard(System.out);
+            // P2.printBoard(System.out);
 
             if (boardEmptyPieces <= 0) {
                 break;
@@ -155,13 +163,13 @@ public class MLPTrainer {
                 NumberofMoves++;
                 lastPlayedMove = P2.makeMove();
                 if (lastPlayedMove.IsPlaceMove) {
-                    System.out.println("Placing to. " + lastPlayedMove.RowPositions[0] + ":" + lastPlayedMove.ColPositions[0] + " by " + lastPlayedMove.P);
+                    //System.out.println("Placing to. " + lastPlayedMove.RowPositions[0] + ":" + lastPlayedMove.ColPositions[0] + " by " + lastPlayedMove.P);
                     boardEmptyPieces--;
                 } else {
-                    System.out.println("Jumping from. " + lastPlayedMove.RowPositions[0] + ":" + lastPlayedMove.ColPositions[0] + " by " + lastPlayedMove.P);
+                    //System.out.println("Jumping from. " + lastPlayedMove.RowPositions[0] + ":" + lastPlayedMove.ColPositions[0] + " by " + lastPlayedMove.P);
                     for (int i = 1; i < lastPlayedMove.ColPositions.length; i++) {
                         boardEmptyPieces--;
-                        System.out.println("To. " + lastPlayedMove.RowPositions[i] + ":" + lastPlayedMove.ColPositions[i]);
+                        //System.out.println("To. " + lastPlayedMove.RowPositions[i] + ":" + lastPlayedMove.ColPositions[i]);
                     }
 
                 }
@@ -173,28 +181,42 @@ public class MLPTrainer {
                     System.exit(1);
                 }
 
-                P1.printBoard(System.out);
-                P2.printBoard(System.out);
+                //P1.printBoard(System.out);
+                //P2.printBoard(System.out);
 
             }
 
         }
-        System.out.println("--------------------------------------");
-        System.out.println("P2 Board is :");
-        P2.printBoard(System.out);
-        System.out.println("P1 Board is :");
-        P1.printBoard(System.out);
+        //System.out.println("--------------------------------------");
+        //System.out.println("P2 Board is :");
+        //P2.printBoard(System.out);
+        //System.out.println("Board is :");
+        //P1.printBoard(System.out);
 
-        System.out.println("Player one (White) indicate winner as: " + P1.getWinner());
-        System.out.println("Player two (Black) indicate winner as: " + P2.getWinner());
-        System.out.println("Total Number of Moves Played in the Game: " + NumberofMoves);
-        System.out.println("Referee Finished !");
+        String winner = "";
+
+
+        switch (P1.getWinner()) {
+            case 1:
+                winner = "White";
+                break;
+            case 2:
+                winner = "Black";
+                break;
+            case 3:
+                winner = "Draw";
+                break;
+        }
+
+        return winner;
+        
+        //System.out.println("Player two (Black) indicate winner as: " + P2.getWinner());
+        //System.out.println("Total Number of Moves Played in the Game: " + NumberofMoves);
+        //System.out.println("Referee Finished !");
     }
 
-    public static void main(String args[]){
+    public static void main(String args[]) {
         MLPTrainer trainer = new MLPTrainer();
         trainer.run();
     }
-    
-    
 }

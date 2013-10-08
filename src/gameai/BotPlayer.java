fcb8fa0798ae7fc28a6132a5c7814ea0f785580e
Mitchell Brunton <mmbrunton@gmail.com>
2013-10-08 16:30:03 +1100

    merged

---
+++ src/gameai/BotPlayer.java
@@ -0,0 +1,89 @@
+package gameai;
+import java.io.PrintStream;
+
+/* Implementation of Player interface */
+
+//import csproj.jumper.*
+
+public class BotPlayer implements Player, Piece {
+	GameBoardMitch gb;
+    int player;
+    boolean illegalOpponentMoveFlag;
+    MoveFinder moveFinder;
+    public static final int SUCCESS = 0;
+    public static final int FAILURE = -1;
+
+    /* initialise empty nxn Gameboard, set player colour and illegal
+     * move flag, initialise moveFinder object for selecting moves
+     */
+    public int init(int n, int p) {
+        if (n <= 0 || (p != WHITE && p != BLACK)) {
+            return FAILURE;
+        }
+
+        gb = new GameBoardMitch(n);
+        player = p;
+        illegalOpponentMoveFlag = false;
+        moveFinder = new MinimaxAlphaBetaMoveFinder(n, p);
+        
+        return SUCCESS;
+    }
+
+    /* Decide on a move to make, apply it to Gameboard, and
+     * return Move object
+     */
+    public Move makeMove() {
+    	Move m = moveFinder.getMove(gb);
+    	gb.applyMove(m);
+    	return m;
+    }
+
+    /* check if opponent's move is legal, and if so apply it to Gameboard,
+     * otherwise, set "illegal move" flag
+     */
+    public int opponentMove(Move m) {
+
+    	if (gb.isLegalMove(m)) {
+    		gb.applyMove(m);
+    		return SUCCESS;
+    	} else {
+    		illegalOpponentMoveFlag = true;
+    		return FAILURE;
+    	}
+    }
+
+    /* return state of gameplay - if opponent has made illegal move, return
+     * "invalid", otherwise return value indicating a white/black victory, draw
+     * or unfinished game
+     */
+    public int getWinner() {
+    	if (illegalOpponentMoveFlag) {
+    		return INVALID;
+    	} else {
+    		return gb.getWinner();
+    	}
+    }
+
+    /* print string representation of Gameboard to desired
+     * output stream
+     */
+    public void printBoard(PrintStream output) {
+    	output.println(gb.toString());
+    }
+    
+    
+    /* functions to help with optimisation
+     */
+    public void setBoard(GameBoardMitch gb) {
+    	this.gb = gb;
+    }
+    
+    public void setMoveFinder(MoveFinder moveFinder) {
+    	this.moveFinder = moveFinder;
+    }
+    
+    public void setMaxDepth(int maxDepth) {
+    	moveFinder.setMaxDepth(maxDepth);
+    }
+}
+
---
+++ src/gameai/Piece.java
@@ -0,0 +1,17 @@
+package gameai;
+//package csproj.jumper;
+/*
+ *   Piece:
+ *      Define types of pieces that can appear on a board
+ *      
+ *   @author msalehi
+ *   
+ */
+
+public interface Piece {
+    public static final int WHITE = 1, 
+                            BLACK = 2,
+                            DEAD = 3,
+                            EMPTY = 0,
+    		                INVALID = -1;
+}
